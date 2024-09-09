cb0e4dc21c8f2676f5aba58483874bc61b5e52be
bealun <53275597+bealun@users.noreply.github.com>
2024-09-09 11:21:46 +0200

    update to handle commands from file input

--- RobotSimulator/Program.cs
+++ RobotSimulator/Program.cs
@@ -5,46 +5,120 @@ namespace RobotSimulator;
 
 public class Program
 {
-    static void Main()
+    private const string DefaultFileDirectory = "RobotSimulator/SampleInputs";
+    
+    static void Main(string[] args)
     {
         Robot robot = new Robot();
         RobotSimulatorService robotService = new RobotSimulatorService(robot);
 
         DisplayUserInstructions();
-        
+
+        try
+        {
+            RunInteractiveMode(robotService);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+    
+    private static void RunInteractiveMode(RobotSimulatorService robotService)
+    {
         while (true)
         {
-            string? command = Console.ReadLine()?.Trim();
-            if (string.IsNullOrEmpty(command))
+            Console.WriteLine("Enter commands or type 'file' to input a file path:");
+            string? input = Console.ReadLine()?.Trim();
+
+            if (string.IsNullOrEmpty(input))
             {
                 HandleEmptyCommand();
                 continue;
             }
 
-            CommandType commandType = ParseCommandType(command, out string[] commandParts);
+            if (input.Equals("file", StringComparison.OrdinalIgnoreCase))
+            {
+                HandleFileSelection(robotService);
+                continue;
+            }
+
+            var (commandType, commandParts) = ParseCommand(input);
 
             if (commandType == CommandType.Exit)
             {
                 Console.WriteLine("Exiting...");
                 break;
             }
-            
+
             ProcessCommand(commandType, commandParts, robotService);
         }
     }
     
-    static CommandType ParseCommandType(string command, out string[] commandParts)
+    private static void HandleFileSelection(RobotSimulatorService robotService)
     {
-        commandParts = command.Split(',');
-
-        if (commandParts.Length == 0)
+        Console.WriteLine("Enter the filename (e.g., test1.txt):");
+        
+        string? fileName = Console.ReadLine()?.Trim();
+        if (string.IsNullOrWhiteSpace(fileName))
         {
-            return CommandType.Unknown;
+            Console.WriteLine("Filename cannot be empty. Please provide a valid filename.");
+            return;
+        }
+        
+        string filePath = Path.Combine(DefaultFileDirectory, fileName);
+
+        if (!string.IsNullOrEmpty(fileName) && File.Exists(filePath))
+        {
+            ProcessCommandsFromFile(filePath, robotService);
+        }
+        else
+        {
+            Console.WriteLine($"File not found: {filePath}");
+        }
+    }
+    
+    public static void ProcessCommandsFromFile(string filePath, RobotSimulatorService robotService)
+    {
+        string[] commands = File.ReadAllLines(filePath);
+        
+        if (commands.Length == 0)
+        {
+            Console.WriteLine("The file is empty. No commands to process.");
+            return;
         }
 
-        return Enum.TryParse<CommandType>(commandParts[0].Trim(), true, out var commandType)
-            ? commandType
-            : CommandType.Unknown;
+        foreach (string command in commands)
+        {
+            if (string.IsNullOrWhiteSpace(command))
+            {
+                Console.WriteLine("Empty command in file. Skipping...");
+                continue;
+            }
+
+            var (commandType, commandParts) = ParseCommand(command);
+
+            if (commandType == CommandType.Exit)
+            {
+                Console.WriteLine("Exiting...");
+                break; 
+            }
+
+            ProcessCommand(commandType, commandParts, robotService);
+        }
+    }
+    
+    static (CommandType, string[]) ParseCommand(string command)
+    {
+        string[] commandParts = command.Split(',');
+        CommandType commandType = CommandType.Unknown;
+
+        if (commandParts.Length > 0)
+        {
+            Enum.TryParse(commandParts[0].Trim(), true, out commandType);
+        }
+
+        return (commandType, commandParts);
     }
 
     static void ProcessCommand(CommandType commandType, string[] commandParts, RobotSimulatorService robotService)
@@ -52,23 +126,7 @@ public class Program
         switch (commandType)
         {
             case CommandType.Place:
-                if (commandParts.Length == 4 &&
-                    int.TryParse(commandParts[1], out var x) &&
-                    int.TryParse(commandParts[2], out var y) &&
-                    Enum.TryParse<Direction>(commandParts[3], true, out var direction))
-                {
-                    bool success = robotService.Place(x, y, direction);
-                    
-                    if (success)
-                        Console.WriteLine($"Robot placed at ({x}, {y}) facing {direction}.");
-                    else
-                        Console.WriteLine("Invalid position. Coordinates must be within the 5x5 grid (0-4)."
-                            + " Please try again.");
-                }
-                else
-                {
-                    Console.WriteLine("Invalid PLACE command format. Example: PLACE,X,Y,F");
-                }
+                ProcessPlaceCommand(commandParts, robotService);
                 break;
 
             case CommandType.Move:
@@ -97,10 +155,28 @@ public class Program
         }
     }
     
+    static void ProcessPlaceCommand(string[] commandParts, RobotSimulatorService robotService)
+    {
+        if (commandParts.Length == 4 &&
+            int.TryParse(commandParts[1], out int x) &&
+            int.TryParse(commandParts[2], out int y) &&
+            Enum.TryParse<Direction>(commandParts[3], true, out Direction direction))
+        {
+            bool success = robotService.Place(x, y, direction);
+
+            if (success)
+                Console.WriteLine($"Robot placed at ({x}, {y}) facing {direction}.");
+            else
+                Console.WriteLine("Invalid position. Coordinates must be within the 5x5 grid (0-4). Please try again.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid PLACE command format. Example: PLACE,X,Y,F");
+        }
+    }
     
     private static void DisplayUserInstructions()
     {
-        
         Console.WriteLine("Welcome to the Robot Simulator!");
         Console.WriteLine("You can control the robot within a 5x5 grid using the following commands:");
         Console.WriteLine("PLACE,X,Y,F - Place the robot at position (X, Y) facing direction F.");
@@ -111,6 +187,7 @@ public class Program
         Console.WriteLine("REPORT - Output the current position and direction of the robot.");
         Console.WriteLine();
         Console.WriteLine("To exit the application, type EXIT.");
+        Console.WriteLine("To process a file, type 'file' and provide the file path when prompted.");
         Console.WriteLine("Example commands:");
         Console.WriteLine("   PLACE,0,0,NORTH");
         Console.WriteLine("   MOVE");
